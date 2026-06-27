40e5cc505d110aeab15bd4a6b1bf901961c1fc2a
AreebaSajjad <areebasajjad568@gmail.com>
2026-06-28 02:32:37 +0500

    Multi Hospital Patient Record System with MongoDB Atlas

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: `${import.meta.env.VITE_API_URL || 'http://localhost:5001'}/api` });
 
 // Har request se pehle token lagao
 API.interceptors.request.use((config) => {
